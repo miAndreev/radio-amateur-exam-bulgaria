3dc56e362a025f35f0d40ecef1f9dd531e121bab
Your Name <you@example.com>
2020-06-12 23:09:42 +0300

    Move common js parts to master

--- js/master.js
+++ js/master.js
@@ -1,5 +1,32 @@
 // Put all your page JS here
 
+var quizJSON = {
+    "info": {
+        "name":    "Подготовка за изпит<br>Радиолюбител клас " + razdel_number,
+        "main":    "<p>80% или 48 от 60 въпроса за да минеш.</p>",
+        "results": "",
+        "level1":  "Отлично!",
+        "level2":  "Минаваш",
+        "level3":  "За една бройка",
+        "level4":  "Можеш по добре",
+        "level5":  "Слабо...." // no comma here
+    },
+    "questions": []
+};
+
+// Fisher-Yates shuffle - разбърква въпросите по добре
+$.fn.shuffle = function() {
+    for (var i = this.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+    }
+    return this;
+}
+
+quizJSON.questions = $(razdel).shuffle().slice(0,60).get();
+
 $(function () {
     $('#slickQuiz').slickQuiz();
 });
